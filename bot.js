dbf2b6295fdcf247ad583a6a0add6356a237a77d
Burhan Fauzi S <burhanfauzisalam@gmail.com>
2023-05-28 15:55:24 +0700

    Bot Whatsapp Autosend

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on("ready", () => {
 client.on("ready", () => {
   // Tentukan jeda waktu (dalam milidetik)
   const rand = Math.floor(Math.random() * 10) * 1000;
-  //   const delay = rand;
+
   // Buat variabel untuk menyimpan indeks saat ini
   let index = 0;
   // Data
@@ -33,8 +33,6 @@ client.on("ready", () => {
       .get(LINK)
       .then(function (response) {
         const arr = response.data;
-        // const cari = arr.find((cari) => cari.induk == "X-110002");
-        // console.log(arr);
         for (let i = 0; i < arr.length; i++) {
           const element = arr[i].no_wa;
           numbr.push(parseInt(element));
@@ -78,21 +76,6 @@ client.on("ready", () => {
       });
   }
   run();
-  // Buat fungsi yang akan dijalankan secara berulang-ulang dengan jeda waktu tertentu
-  //   function repeat() {
-  //     client.sendMessage(
-  //       `${numbr[index]}@c.us`,
-  //       `Selamat siang Bpk/Ibu pelanggan setia *FIFGROUP*\nKarena pembayaran bpk/ibu bagus, maka saya mau menawarkan untuk top up atau pengajuan pinjaman dana.\nUntuk pengajuan di tgl 28-31 Mei 2023, jatuh tempo di awal bulan Juli 2023\n\nInfo marketing:\n085707947308 (Burhan)\nCover Area Surabaya-Sidoarjo*`
-  //     );
-  //     console.log(`Pesan ke-${numbr[index]} berhasil terkirim`);
-  //     index++;
-  //     // Cek apakah perulangan sudah selesai, jika belum, jalankan fungsi setTimeout() kembali
-  //     if (index < numbr.length) {
-  //       setTimeout(repeat, delay);
-  //     }
-  //   }
-  //   // Panggil fungsi pertama kali untuk memulai perulangan
-  //   repeat();
 });
 
 client.initialize();
