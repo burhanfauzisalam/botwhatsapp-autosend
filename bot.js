e698b131a8eeca609027cec0ae609e6e935a2772
Burhan Fauzi S <burhanfauzisalam@gmail.com>
2023-06-01 16:53:56 +0700

    Autosend

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 require("dotenv").config();
 const LINK = process.env.LINK;
 const TEXT = process.env.TEXT;
 
-const { Client, LocalAuth } = require("whatsapp-web.js");
+//Authentication
 const client = new Client({
   authStrategy: new LocalAuth(),
 });
@@ -23,59 +24,62 @@ client.on("ready", () => {
   // Tentukan jeda waktu (dalam milidetik)
   const rand = Math.floor(Math.random() * 10) * 1000;
 
-  // Buat variabel untuk menyimpan indeks saat ini
-  let index = 0;
   // Data
   const numbr = [];
+  const arr = [];
 
-  function run() {
+  // Fungsi autosend
+  function autoSend() {
+    // Menentukan nilai awal index
+    let index = 0;
+
+    // Mengambil data dari API
     axios
       .get(LINK)
       .then(function (response) {
-        const arr = response.data;
-        for (let i = 0; i < arr.length; i++) {
-          const element = arr[i].no_wa;
-          numbr.push(parseInt(element));
+        const data = response.data;
+        for (let a = 0; a < data.length; a++) {
+          arr.push(data[a]);
+          numbr.push(parseInt(arr[a].no_wa));
         }
+        console.log("Mengambil data dari API...");
         console.log(numbr);
-        //   Buat fungsi yang akan dijalankan secara berulang-ulang dengan jeda waktu tertentu
-        function repeat() {
-          if (numbr.length > 0) {
-            client.sendMessage(`${numbr[index]}@c.us`, TEXT);
-            console.log(`Pesan ke-${numbr[index]} berhasil terkirim`);
-            index++;
-            // Cek apakah perulangan sudah selesai, jika belum, jalankan fungsi setTimeout() kembali
-            if (index < numbr.length) {
-              setTimeout(repeat, rand);
-            } else {
-              console.log(`${numbr.length} pesan telah terkirim`);
-              for (let del = 0; del < numbr.length; del++) {
-                axios
-                  .delete(`${LINK}/${del}`)
-                  .then((response) => {
-                    console.log("Data deleted successfully");
-                  })
-                  .catch((error) => {
-                    console.error("Error deleting data:", error);
-                  });
-              }
-              numbr.splice(0, numbr.length);
-              index = 0;
-              setTimeout(run, 3000);
-            }
+      })
+      .then(function send() {
+        // Menjalankan fungsi mengirim pesan
+        if (numbr.length == 0) {
+          // Jika data kosong, maka akan diperiksa ulang
+          setTimeout(autoSend, 15000);
+          console.log("Tidak ada data");
+          console.log("Memeriksa ulang data dari API...");
+        } else {
+          // Jika ada datanya, maka pesan akan dikirimkan
+          client.sendMessage(`${numbr[index]}@c.us`, TEXT);
+          console.log(`Pesan ke-${numbr[index]} berhasil terkirim`);
+          // Setelah mengirim pesan, data dari API akan dihapus
+          axios
+            .delete(`${LINK}/0`)
+            .then((response) => {})
+            .catch((error) => {
+              console.error("Error menghapus data:", error);
+            });
+          index++;
+          // Memeriksa apakah pesan sudah dikirimkan ke semua data, jika belum maka menjalankan fungsi send() kembali
+          if (index < numbr.length) {
+            setTimeout(send, rand);
           } else {
-            console.log("Tidak ada data");
-            setTimeout(run, 300000);
+            // Jika pesan sudah terkirim semua, maka semua data akan direset dan memeriksa ulang data dari API
+            console.log(`${numbr.length} pesan telah terkirim.`);
+            console.log("Memeriksa data dari API...");
+            numbr.splice(0);
+            arr.splice(0);
+            setTimeout(autoSend, 15000);
           }
         }
-        // Panggil fungsi pertama kali untuk memulai perulangan
-        repeat();
-      })
-      .catch((e) => {
-        console.log(e);
       });
   }
-  run();
+  // Menjalankan fungsi autosend
+  autoSend();
 });
 
 client.initialize();
